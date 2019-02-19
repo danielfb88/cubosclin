8f1055eaf9a55487a1567272a1dcc538d0d319b5
Daniel Bonfim <daniel.fb88@gmail.com>
2019-02-19 17:28:35 -0300

    skeleton de testes unitários

---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,29 @@
+import { testdouble, expect } from './config/helpers';
+
+describe('Testes Unitários do Controller', () => {
+
+    describe('Método Create', () => {
+        it('Deve criar um novo Objeto', () => {
+
+        });
+    });
+
+    describe('Método Update', () => {
+        it('Deve atualizar um novo Objeto', () => {
+
+        });
+    });
+
+    describe('Método Get', () => {
+        it('Deve retornar uma lista com todos os objetos', () => {
+
+        });
+    });
+
+    describe('Método Delete', () => {
+        it('Deve deletar um Objeto', () => {
+
+        });
+    });
+
+})
