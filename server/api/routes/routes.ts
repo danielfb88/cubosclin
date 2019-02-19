eecbb74e7ad34e85bb3272903eb9b799b733fc08
Daniel Bonfim <daniel.fb88@gmail.com>
2019-02-19 18:36:17 -0300

    Refatorando rotas - Skeleton de RegraHorarioRoutes

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,13 +1,21 @@
 import { Application, Request, Response } from 'express';
+import RegraHorarioRoutes from '../../modules/RegraHorario/routes';
 
 class Routes {
+
+    private router: RegraHorarioRoutes;
+
     constructor(app: Application) {
+        this.router = new RegraHorarioRoutes();
         this.getRoutes(app);
     }
 
     getRoutes(app: Application): void {
-        app.route('/').get((req: Request, res: Response) => res.send('Hello, world!'));
-        app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`))
+        app.route('/api/regrahorario/all').get(this.router.findAll);
+        app.route('/api/regrahorario/:id').get(this.router.findOne);
+        app.route('/api/regrahorario/create').post(this.router.create);
+        app.route('/api/regrahorario/:id/update').put(this.router.update);
+        app.route('/api/regrahorario/:id/delete').delete(this.router.delete);
     }
 }
 
---
+++ server/modules/RegraHorario/routes.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+class RegraHorarioRoutes {
+    constructor() { }
+
+    findAll(req: Request, res: Response) {
+
+    }
+
+    findOne(req: Request, res: Response) {
+
+    }
+
+    create(req: Request, res: Response) {
+
+    }
+
+    update(req: Request, res: Response) {
+
+    }
+
+    delete(req: Request, res: Response) {
+
+    }
+
+}
+
+export default RegraHorarioRoutes;
