bb2c4bb51a48a2401773c3f894d7d7c16969d126
Daniel Bonfim <daniel.fb88@gmail.com>
2019-02-19 22:37:39 -0300

    Ajuste da classe de testes de integração

--- tests/integration/integration.test.ts
+++ tests/integration/integration.test.ts
@@ -2,69 +2,74 @@ import { app, request, expect } from './config/helpers';
 
 describe('Testes de integração', () => {
 
-    describe('GET /api/all', () => {
+    describe('GET /api/regrahorario/all', () => {
         it('Deve retornar um json com todos os OBJETOS', done => {
 
             request(app)
-                .get('/api/all')
+                .get('/api/regrahorario/all')
                 .end((error, res) => {
                     expect(res.status).to.equals(200);
+                    done(error);
                 })
 
         });
     });
 
-    describe('GET /api/:id', () => {
+    describe('GET /api/regrahorario/:id', () => {
         it('Deve retornar um json com um OBJETO', done => {
 
             request(app)
-                .get(`/api/${1}`)
+                .get(`/api/regrahorario/${1}`)
                 .end((error, res) => {
                     expect(res.status).to.equals(200);
+                    done(error);
                 })
 
         });
     });
 
-    describe('POST /api/new', () => {
+    describe('POST /api/regrahorario/create', () => {
         it('Deve criar um OBJETO', done => {
 
             const obj = {
                 nome: 'Teste'
             }
             request(app)
-                .post('/api/new')
+                .post('/api/regrahorario/create')
                 .send(obj)
                 .end((error, res) => {
                     expect(res.status).to.equals(200);
+                    done(error);
                 })
 
         });
     });
 
-    describe('PUT /api/:id/edit', () => {
+    describe('PUT /api/regrahorario/:id/update', () => {
         it('Deve editar um OBJETO', done => {
 
             const obj = {
                 nome: 'TesteUpdate'
             }
             request(app)
-                .post(`/api/${1}/edit`)
+                .post(`/api/regrahorario/${1}/update`)
                 .send(obj)
                 .end((error, res) => {
                     expect(res.status).to.equals(200);
+                    done(error);
                 })
 
         });
     });
 
-    describe('DELETE /api/:id', () => {
+    describe('DELETE /api/regrahorario/:id/delete', () => {
         it('Deve deletar um OBJETO', done => {
 
             request(app)
-                .delete(`/api/${1}`)
+                .delete(`/api/regrahorario/${1}/delete`)
                 .end((error, res) => {
                     expect(res.status).to.equals(200);
+                    done(error);
                 })
 
         });
